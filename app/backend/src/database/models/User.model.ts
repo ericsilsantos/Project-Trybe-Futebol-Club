c3b5ea5dfbfaff390b8a88dc4e225746266f1b12
Eric Santos <eric.silvasantos44@gmail.com>
2022-08-18 10:04:46 -0300

    feat: primeiro requisito

---
+++ app/backend/src/database/models/User.model.ts
@@ -0,0 +1,38 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  passwor: string;
+}
+
+Users.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  timestamps: false,
+});
+
+export default Users;
